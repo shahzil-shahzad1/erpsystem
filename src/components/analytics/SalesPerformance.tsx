3d1abbcf46e533e34309281f915eb832dcb26f69
Shahzil Shahzad <silentboys1117@gmail.com>
2025-07-31 20:20:13 +0500

    Update SalesPerformance.tsx

--- src/components/analytics/SalesPerformance.tsx
+++ src/components/analytics/SalesPerformance.tsx
@@ -7,13 +7,13 @@ ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, BarEleme
 
 import { SalesData } from '@/lib/analytics/chart';
 
-const SalesPerformance = ({salesData} : SalesData) => {
+const SalesPerformance = ({salesByCategoryData} : SalesData) => {
   return (
    <div className="inset-0 bg-white/90 dark:bg-black/80 backdrop-blur-md rounded-xl shadow-lg p-6 border border-gray-200 dark:border-zinc-900">
              <h2 className="text-lg font-bold mb-4 text-zinc-900 dark:text-white">Sales Performance by Category</h2>
              <p className="text-sm text-gray-600 dark:text-gray-300 mb-6">Sales distribution across different product categories.</p>
              <div className="h-64">
-               <Bar data={salesData} />
+               <Bar data={salesByCategoryData} />
              </div>
            </div>
   )
