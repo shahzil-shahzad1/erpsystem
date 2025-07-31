26aa98ee821ef619e722c40db965ec871da1d7fd
Shahzil Shahzad <silentboys1117@gmail.com>
2025-07-31 20:19:49 +0500

    Update KeyFinancialTrends.tsx

--- src/components/analytics/KeyFinancialTrends.tsx
+++ src/components/analytics/KeyFinancialTrends.tsx
@@ -7,13 +7,13 @@ ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, BarEleme
 
 import { ChartData } from '@/lib/analytics/chart';
 
-const KeyFinancialTrends = ({ financialdata }: ChartData) => {
+const KeyFinancialTrends = ({ financialData }: ChartData) => {
   return (
     <div className="inset-0 bg-white/90 dark:bg-black/80 backdrop-blur-md rounded-xl shadow-lg p-6 border border-gray-200 dark:border-zinc-900">
       <h2 className="text-lg font-bold mb-4 text-zinc-900 dark:text-white">Key Financial Trends</h2>
       <p className="text-sm text-gray-600 dark:text-gray-300 mb-6">Revenue, gross profit, and net profit over time.</p>
       <div className="h-64">
-        <Line data={financialdata} />
+        <Line data={financialData} />
       </div>
     </div>
   )
