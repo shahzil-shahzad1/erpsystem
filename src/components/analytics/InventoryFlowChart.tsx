44ed814fcc7936bdcd72035fd8bcd262f3a8f777
Shahzil Shahzad <silentboys1117@gmail.com>
2025-07-31 18:14:58 +0500

    Update InventoryFlowChart.tsx

--- src/components/analytics/InventoryFlowChart.tsx
+++ src/components/analytics/InventoryFlowChart.tsx
@@ -11,7 +11,7 @@ interface KeyFinancialTrendsProps {
   financialTrendData: ChartData;
 }
 
-const InventoryFlowChart = ({inventoryFlowData} : {inventoryFlowData: KeyFinancialTrendsProps}) => {
+const InventoryFlowChart = ({inventoryFlowData} : KeyFinancialTrendsProps) => {
   return (
    <div className="inset-0 bg-white/90 dark:bg-black/80 backdrop-blur-md rounded-xl shadow-lg p-6 border border-gray-200 dark:border-zinc-900">
              <h2 className="text-lg font-bold mb-4 text-zinc-900 dark:text-white">Inventory Flow & Value Trend</h2>
