399bd6c7e1b3ef3d87bb12d66416c4e5581acebb
Shahzil Shahzad <silentboys1117@gmail.com>
2025-07-31 20:21:04 +0500

    Update WorkforePayrollChart.tsx

--- src/components/analytics/WorkforePayrollChart.tsx
+++ src/components/analytics/WorkforePayrollChart.tsx
@@ -7,17 +7,13 @@ ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, BarEleme
 
 import { ChartData } from '@/lib/analytics/chart';
 
-interface KeyFinancialTrendsProps {
-    financialTrendData: ChartData;
-}
-
-const WorkforcePayrollChart = ({ hrcostdata }: KeyFinancialTrendsProps) => {
+const WorkforcePayrollChart = ({ hrCostTrendData }: ChartData) => {
     return (
         <div className="inset-0 bg-white/90 dark:bg-black/80 backdrop-blur-md rounded-xl shadow-lg p-6 border border-gray-200 dark:border-zinc-900">
             <h2 className="text-lg font-bold mb-4 text-zinc-900 dark:text-white">Workforce & Payroll Cost Trend</h2>
             <p className="text-sm text-gray-600 dark:text-gray-300 mb-6">Active employees and total payroll expenses over time.</p>
             <div className="h-64">
-                <Line data={hrcostdata} />
+                <Line data={hrCostTrendData} />
             </div>
         </div>
     )
